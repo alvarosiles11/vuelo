df21b4c4c536657aff65106197d6a78f7ce94884
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2022-09-30 23:24:13 -0400

    🥇
    🥇

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+#npx prettier --check .    # Ejecuto el prettier para verificar el orden del codigo
+#npx prettier --write .    # Ejecuto el prettier para ordenar el codigo
 #NOTA permisos file.sh
 #chmod u+r+x deploy.sh
 
